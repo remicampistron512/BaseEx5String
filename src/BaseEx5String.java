5eaeaebbf9e1863620571caed52b1cb359583b41
Rémi Campistron <remi.campistron@gmail.com>
2025-10-29 11:44:58 +0100

    Troisième partie de l'exercice, remplacer un mot dans une phrase

--- src/BaseEx5String.java
+++ src/BaseEx5String.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class BaseEx5String {
 
@@ -17,14 +18,47 @@ public class BaseEx5String {
 		String sentence = "il fait beau aujourd'hui";
 		String [] sentenceArray =  sentence.split(" ");
 		
+		
+		
+		
+		// troisième partie, utiliser la saisie utilisateur pour remplacer un mot
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Dans la phrase suivante : " + sentence );
+		System.out.println("entrez un mot à trouver");
+		wordToFind = scan.nextLine();
+		
+		findWord(wordToFind, sentenceArray);
+		
+		System.out.println("par quel mot voulez vous le remplacer ?");
+		
+		String replacementWord = scan.nextLine();
+		String joinedSentence = replaceWord(wordToFind,replacementWord, sentenceArray);
+		
+		
+		System.out.println(joinedSentence);
+		
+		
+	}
+
+	public static void findWord(String wordToFind, String[] sentenceArray) {
 		for (int i = 0; i<sentenceArray.length;i++) {
 			if (sentenceArray[i].equals(wordToFind)) {
 				System.out.println("trouvé");
 			}
 		}
+	}
+	
+	public static String replaceWord(String wordToFind, String replacementWord,String[] sentenceArray ) {
+		for (int i = 0; i<sentenceArray.length;i++) {
+			if (sentenceArray[i].equals(wordToFind)) {
+				sentenceArray[i] = replacementWord;
+			}
+		}
+		String joinedSentence = String.join(" ",sentenceArray);
 		
-		
-		
+		return joinedSentence;
 	}
 
 }
