d96bb083995734cc44304ac808268bb7da8d06fc
Rémi Campistron <remi.campistron@gmail.com>
2025-10-29 11:19:14 +0100

    Première partie de l'exercice, deux façons de concatener deux strings

--- src/BaseEx5String.java
+++ src/BaseEx5String.java
@@ -2,8 +2,19 @@
 public class BaseEx5String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Premiere partie de l'exercice, deux façons de concatener deux strings entre elles
+		String a = "Premiere phrase";
+		String b = "Deuxième phrase";
+		
+		System.out.println(a + "," + b);
+		
+		System.out.println(a.concat(",").concat(b));
+		
+		
+		
+		
+		
+		
 	}
 
 }
